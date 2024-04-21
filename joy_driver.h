05f5a3e430b6d996edf42bb9a278bbbc02ea2ba2
BigFloppa456 <81697550+BigFloppa456@users.noreply.github.com>
2024-04-21 07:46:07 +0530

    Update joy_driver.h
    removed unnecessary comments

--- joy_driver.h
+++ joy_driver.h
@@ -1,22 +1,6 @@
 #ifndef JOY_DRIVER_H
 #define JOY_DRIVER_H
 
-// #include <linux/joystick.h>
-
-/*  Controller inputs will be read as js_event */
-    // this is defined in linux/joystick.h
-// struct js_event{
-//     _u32 time;  // event timestamp
-//     _s16 value;
-//     _u8 type;   // type of event
-//     _u8 number; // axis/button number
-// };
-
-//     // this is defined in linux/joystick.h
-// #define JS_EVENT_BUTTON 0x01 // button pressed or released
-// #define JS_EVENT_AXIS 0x02 // analog stick
-// #define JS_EVENT_INIT 0x80 // initial state of device
-
 struct joyinp{
     int number;
 	int type;
